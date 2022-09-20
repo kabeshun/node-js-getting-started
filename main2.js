760c809e2d1b93cdccfcb084075ec9b72217af1f
Shungo Kusakabe <shungo12tennis@gmail.com>
2022-09-20 22:57:20 +0900

    script write

--- main2.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  var height = document.getElementsByTagName("html")[0].scrollHeight;
-  window.parent.postMessage(["setHeight", height], "*");
-}
-
-console.error("main2 js file");
