deda8c22ee47f384ad340437cfcec2b436f02839
Shungo Kusakabe <shungo12tennis@gmail.com>
2022-09-20 22:36:37 +0900

    setHeight

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+console.error("index.js")
+window.onload = function() {
+  var height = document.getElementsByTagName("html")[0].scrollHeight;
+  window.parent.postMessage(["setHeight", height], "*");
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
